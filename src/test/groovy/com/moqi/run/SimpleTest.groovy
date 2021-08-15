2ddd5de1e80985dc64b2c7d0bf5f61c56a7f9d12
moqi <moqimoqidea@gmail.com>
2021-08-15 23:38:16 +0800

    Add simple test

---
+++ src/test/groovy/com/moqi/run/SimpleTest.groovy
@@ -0,0 +1,19 @@
+package com.moqi.run
+
+
+import spock.lang.Specification
+
+/**
+ * SimpleTest
+ *
+ * @author moqi
+ * On 2021/8/15 23:21
+ */
+class SimpleTest extends Specification {
+
+    def "simple test"() {
+        expect:
+        1 == 1
+    }
+
+}
