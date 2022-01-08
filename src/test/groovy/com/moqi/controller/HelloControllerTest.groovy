1f0e5e9446b6d5e165ee5477c229c6a89d686597
moqi <moqimoqidea@gmail.com>
2022-01-09 01:30:21 +0800

    Add HelloControllerTest

---
+++ src/test/groovy/com/moqi/controller/HelloControllerTest.groovy
@@ -0,0 +1,28 @@
+package com.moqi.controller
+
+import com.moqi.service.UserService
+import spock.lang.Specification
+
+/**
+ *
+ * @author moqi
+ * @date 1/9/22 01:29
+ */
+class HelloControllerTest extends Specification {
+
+    UserService userService = Mock()
+    HelloController helloController = new HelloController(userService: userService)
+
+    def "Person"() {
+        given:
+        userService.getRandomNumber(*_) >> 666
+
+        when:
+        def personResult = helloController.person(10)
+        println("personResult = $personResult")
+
+        then:
+        personResult.length() > 0
+    }
+
+}
